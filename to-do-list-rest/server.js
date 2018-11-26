a8ad73d29cda990d5e93113d6f437be882b74520
Tej Nandagiri <npsritej@yahoo.co.nz>
2018-11-26 21:23:32 +1300

    Merge BackEnd into master

--- to-do-list-rest/server.js
+++ to-do-list-rest/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (req, res) {
 
@@ -11,6 +20,15 @@ app.get('/', function (req, res) {
 
     res.send(response);
 });
+
+app.post('/test/', function (req, res) {
+    const response = req.body;
+    console.log(response);
+    // console.log("\n\n****REQ****", { res }, "****");
+
+    res.send("post");
+});
+
 
 app.listen(port);
 console.log(`API running on port: ${port}`);
