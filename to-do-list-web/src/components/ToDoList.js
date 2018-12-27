e32d978feeacf8d726f1505b850fc30989f0bf0f
Tej Nandagiri <npsritej@yahoo.co.nz>
2018-12-28 01:17:31 +1300

    To-do-list can now add delete and clear all items

--- to-do-list-web/src/components/ToDoList.js
+++ to-do-list-web/src/components/ToDoList.js
@@ -1,27 +1,85 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+    state = {
+        value: '',
+        items: []
+    }
 
-    submitItem = () => {
-        console.log("CLICKED")
-        const opts = {
-            "first": "testing",
-            "second": "also"
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const currentItem = this.state.value;
+        const newItem = currentItem !== '' && !this.state.items.includes(currentItem);
+
+        if (newItem) {
+            this.setState({
+                value: '',
+                items: [...this.state.items, this.state.value]
+            })
         }
 
-        console.log({opts});
+    }
 
-        fetch('http://localhost:8080/test/', {
-            method: 'post',
-            body: opts
+    handleInputChange = (event) => {
+        this.setState({
+            value: event.target.value
         })
+    }
+
+    handleItemClick = (event) => {
+        const removedItem = event.target.value;
+        const items = this.state.items.filter(item => {
+            return item !== removedItem;
+        })
+
+        this.setState({
+            items: items
+        })
+
+    }
+
+    handleClearAllClick = () => {
+        this.setState({
+            items: []
+        })
+    }
+
+    renderClearAll = () => {
+        return (
+            this.state.items.length > 0
+                ?
+                (
+                    <button onClick={this.handleClearAllClick}>Clear All?</button>
+                )
+                : ''
+        )
     }
 
     render() {
         return (
-            <button style={{ height: 50, width: 100 }} onClick={this.submitItem}>
-                INPUT FIELDS
-            </button>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>What do you need to do today?
+                    <input type="text" value={this.state.value} onChange={this.handleInputChange}></input>
+                    </label>
+                    <input type='submit' value='Add'></input>
+                </form>
+
+                <div style={{ textAlign: 'left' }}>
+                    Current items:  {this.renderClearAll()}
+                    <ul>
+                        {this.state.items.map(item => {
+                            return (
+                                <div style={{ padding: '10px' }} key={item}>
+                                    <button style={{ display: 'inline', marginRight: '20px' }} value={item} onClick={this.handleItemClick}>X</button>
+                                    <li style={{ display: 'inline' }}>{item}</li>
+                                </div>
+                            )
+                        })}
+                    </ul>
+                </div>
+
+            </div>
         )
     }
 }
